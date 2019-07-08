15b0bbd2848ad27873f2d691e17b1c10554bd128
gitman328 <42208677+gitman328@users.noreply.github.com>
2019-07-08 16:30:33 +0200

    Add files via upload

--- parser/rss.php
+++ parser/rss.php
@@ -25,7 +25,7 @@
 	foreach( $rubric as $key => $rubric_desc)
 	{
 	
-	$xmlfile = 'https://derstandard.at/rss/'.$rubric_desc.'';
+	$xmlfile = "https://derstandard.at/rss/".$rubric_desc."";
 	
 	$xml = simplexml_load_file($xmlfile);
 
@@ -33,21 +33,21 @@
     
 	for ($i = 0; $i <= 20; $i++) {
 	
-	if($xml->channel->item[$i]->title == ''){ break; }
+	if($xml->channel->item[$i]->title == ""){ break; }
 
 	$category = trim($xml->channel->item[$i]->category);
 	$title = trim($xml->channel->item[$i]->title);
 	$description = trim($xml->channel->item[$i]->description);
 	$link = $xml->channel->item[$i]->link;
-	$pubdate = utf8_decode($xml->channel->item[$i]->pubDate);
+	$pubdate = $xml->channel->item[$i]->pubDate;
 	
 	// image
 	preg_match_all("#src=\"(.*?)\"#si", $description, $match_image);
 	$image = str_replace("src=\"", "", $match_image[0][0]);
 	$image = str_replace("\"", "", $image);
 
-	$description = str_replace('src="'.$image.'"', "", $description);
-	$description = str_replace("<img style=\"float: right\" >", "", $description);
+	// description
+	$description = strip_tags($description);
 	
 	$image = strtolower($image);
 	
@@ -57,61 +57,44 @@
 	
 	$time = date("H:i", $timestamp);
 	
-	$hash = hash('md4',$link);
+	$hash = hash("md4",$link);
 	
 	if($rubric_desc == ""){ $rubric_desc = "top_news"; }
-
-//	$item_no = $i + 1;
-//	echo $item_no.'<br>';
-//	echo 'Rubrik desc: '.$rubric_desc.'<br>';
-//	echo 'Kategorie: '.$category.'<br>';
-//	echo 'Title: '.$title.'<br>';
-//	echo 'Beschreibung: '.$description.'<br>';
-//	echo 'Image: '.$image.'<br>';
-//	echo 'Link: '.$link.'<br>';
-//	echo 'Datum: '.$date.'<br>';
-//	echo 'Zeit: '.$time.'<br>';
-//	echo 'Timestamp: '.$timestamp.'<br>';
-//	echo 'Hash: '.$hash.'<br>';
-//	echo '<hr>
-//	';
 	
-	$sql_0 = mysqli_query($dbmysqli, "SELECT COUNT(id) FROM `".$rubric_desc."` WHERE `hash` LIKE '".$hash."' ");
-	$result_0 = mysqli_fetch_row($sql_0);
-	$summary = $result_0[0];
-	
-	if($summary == 0)
-	{
-	mysqli_query($dbmysqli, "INSERT INTO `".$rubric_desc."` 
-	(
-	`category`, 
-	`title`, 
-	`description`, 
-	`image`, 
-	`link`, 
-	`pubdate`, 
-	`date`, 
-	`timestamp`, 
-	`hash`
-	) 
-	VALUES 
-	(
-	'".$category."', 
-	'".$title."', 
-	'".$description."', 
-	'".$image."', 
-	'".$link."', 
-	'".$pubdate."', 
-	'".$date."', 
-	'".$timestamp."', 
-	'".$hash."' 
-	)
-	");
+	//$sql_0 = mysqli_query($dbmysqli, "SELECT COUNT(id) FROM `".$rubric_desc."` WHERE `hash` LIKE '".$hash."' ");
+//	$result_0 = mysqli_fetch_row($sql_0);
+//	$summary = $result_0[0];
+//	
+//	if($summary == 0)
+//	{
+//	mysqli_query($dbmysqli, "INSERT INTO `".$rubric_desc."` 
+//	(
+//	`category`, 
+//	`title`, 
+//	`description`, 
+//	`image`, 
+//	`link`, 
+//	`pubdate`, 
+//	`date`, 
+//	`timestamp`, 
+//	`hash`
+//	) 
+//	VALUES 
+//	(
+//	'".$category."', 
+//	'".$title."', 
+//	'".$description."', 
+//	'".$image."', 
+//	'".$link."', 
+//	'".$pubdate."', 
+//	'".$date."', 
+//	'".$timestamp."', 
+//	'".$hash."' 
+//	)
+//	");
+//	}
 	
 	}
-	
-	}
-	
 	}
 	
 	sleep(3);
