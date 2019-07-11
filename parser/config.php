480dfe4ee7d5a2a91237be6d6aee736ba357533d
gitman328 <42208677+gitman328@users.noreply.github.com>
2019-07-11 18:49:04 +0200

    Update config.php

--- parser/config.php
+++ parser/config.php
@@ -4,6 +4,8 @@
 	error_reporting(E_ALL);
 	
 	@$dbmysqli = mysqli_connect("localhost", "sql-benutzername", "sql-passwort", "sql-datenbank");
+	@$dbmysqli->set_charset("utf8");
+
 	if (mysqli_connect_errno())
 	{
 	echo "Failed to connect to MySQL: " . mysqli_connect_error();
